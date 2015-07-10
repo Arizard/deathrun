9c6536a245a913b4449fda401d87451dbb88bc31
Arie Oldman <arieoldman@gmail.com>
2015-07-10 12:46:54 +1000

    Delete sv_round.lua

--- gamemode/sv_round.lua
+++
@@ -1,6 +0,0 @@
-include("shared.lua")
-include("sh_round.lua")
-
--- create network strings for round state changes
-util.AddNetworkString("ROUND_STATE")
--- send this each time round state changes so that the player can update themselves
