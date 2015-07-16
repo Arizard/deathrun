85bda6ba43a625989d226341c84137790e208598
Arizard <arieoldman@gmail.com>
2015-07-16 16:28:53 +1000

    fucked with the knife again

--- entities/weapons/weapon_knife/shared.lua
+++ entities/weapons/weapon_knife/shared.lua
@@ -82,122 +82,38 @@ function SWEP:PrimaryAttack()
 	--if CurTime() < self.LastPrimaryShot + self.Primary.Delay then return end
 	self.LastPrimaryShot = CurTime()
 
+	local bullet = {}
+	bullet.Distance = 75
+	bullet.Force = 5
+	bullet.HullSize = 7
+	bullet.Num = 1
+	bullet.Tracer = 0
+	bullet.Dir = self.Owner:GetAimVector()
+	bullet.Src = self.Owner:GetShootPos()
+	bullet.Attacker = self.Owner
+	bullet.Damage = self.Primary.Damage
 
+	self.Weapon:FireBullets( bullet )
+
+	-- break shit???
+	local tr = self.Owner:GetEyeTrace()
+	if tr.HitPos:Distance( self.Owner:GetShootPos() ) < 75 then
+		if tr.Entity then
+			if SERVER then
+				local keyvalues = tr.Entity:GetKeyValues()
+				if keyvalues.classname == "func_breakable" then
+					tr.Entity:Input("RemoveHealth", self.Owner, self.Weapon, self.Primary.Damage)
+				end
+			end
+		end
+	end
 
 	if ( self.ShootafterTakeout > CurTime() ) then return end		
 	self.Weapon:SetNextPrimaryFire( CurTime() + 0.5 )
 	self.Weapon:SetNextSecondaryFire( CurTime() + 0.5 )
-	local ang = self.Owner:EyeAngles()
-	local spos = self.Owner:EyePos()
-	local tracedata = {
-            start = spos, 
-            endpos = spos + ang:Forward()*75,
-            mins = Vector(-5,-5,-5),
-            maxs = Vector(5,5,5),
-            filter = self.Owner,
-            mask = MASK_SHOT_HULL
-         }
- 	local tr = util.TraceHull( tracedata )
- 	local dir = ((tr.HitPos or Vector(0,0,0)) - self.Owner:EyePos())
- 	dir:Normalize()
- 	dir = dir:Angle()
- 	dir:Normalize()
- 	
 
-		if IsValid( tr.Entity ) then
-			if tr.Entity:IsPlayer() then
-			
-				
-				self.Owner:SetAnimation( PLAYER_ATTACK1 );
-				self.Weapon:SendWeaponAnim( ACT_VM_PRIMARYATTACK );
-
-				
-				--find the direction
-				local newHitPos = tr.Entity:NearestPoint( tr.HitPos )
-
-				local customDirection = (newHitPos - self.Owner:GetShootPos())
-				customDirection:Normalize()
-
-				--print( customDirection:Dot( self.Owner:GetAimVector() ) )
-				if SERVER then
-				
-					local dmginfo = DamageInfo()
-					dmginfo:SetDamage( self.Primary.Damage )
-					dmginfo:SetDamageType( DMG_SLASH )
-
-					if tr.Entity:IsPlayer() then
-						if self.Owner:Team() == tr.Entity:Team() then
-							dmginfo:SetDamage( 0 )
-						end
-					end
-					tr.Entity:TakeDamageInfo( dmginfo )
-
-					if tr.Entity:IsPlayer() then
-						if self.hit == 1 then
-							self.Owner:EmitSound( self.FleshHit1 )
-							self.hit = 2
-							
-						elseif self.hit == 2 then
-							self.Owner:EmitSound( self.FleshHit2 )
-							self.hit = 3
-							
-						elseif self.hit == 3 then
-							self.Owner:EmitSound( self.FleshHit3 )
-							self.hit = 4
-							
-						else
-							self.Owner:EmitSound( self.FleshHit4 )
-							self.hit = 1
-						end
-					else
-						self.Owner:EmitSound(self.WallSound,100,math.random(95,110))
-					end
-
-				end
-			else
-				if SERVER then
-					local bullet = {} -- from weapon_base
-				
-					bullet.Num 	= 1
-					bullet.Src 	= self.Owner:GetShootPos() -- Source
-					bullet.Dir 	= self.Owner:GetAimVector() -- Dir of bullet
-					bullet.Spread 	= Vector( 0,0, 0 )	-- Aim Cone
-					bullet.Tracer	= 0 -- Show a tracer on every x bullets
-					bullet.Force	= 5 -- Amount of force to give to phys objects
-					bullet.Damage	= self.Primary.Damage
-					bullet.AmmoType = ""
-
-					self.Owner:FireBullets( bullet )
-					self.Owner:EmitSound(self.WallSound,100,math.random(95,110))
-				end
-
-			end
-				
-		else
-			
-			local tr = self.Owner:GetEyeTrace()
-			if tr.HitPos:Distance( self.Owner:GetShootPos() ) < 75 then
-				
-
-				
-
-				if SERVER then
-					self.Owner:EmitSound(self.WallSound,100,math.random(95,110))
-				end
-			else
-				if SERVER then
-					self.Owner:EmitSound(self.MissSound,100,math.random(90,120))
-				end
-			end
-			
-		end
-
-	-- decal
-	local tr = self.Owner:GetEyeTrace()
-	if tr.HitPos:Distance( self.Owner:GetShootPos() ) < 75 then
-		util.Decal("ManhackCut", tr.HitPos + tr.HitNormal, tr.HitPos - tr.HitNormal)
-	end
-	
+	self.Owner:SetAnimation( PLAYER_ATTACK1 );
+	self.Weapon:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
 	
 	self.Owner:SetAnimation( PLAYER_ATTACK1 ) --3rd Person Animation
 	self.Weapon:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
