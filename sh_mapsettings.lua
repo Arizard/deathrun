fb2f6b49e24c36c5af98db4001085bc166413f13
Hainaa <dayle.ashton@hotmail.com>
2016-05-26 11:18:41 +0800

    I fukt up

--- sh_mapsettings.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-	for k,v in ipairs( file.Find("gamemode/mapsettings/*.lua", "LUA") ) do
-		AddCSLuaFile("mapsettings/"..v)
-	end
-end
-
-hook.Add("InitPostEntity", "MapSettings", function()
-	include("mapsettings/"..game.GetMap()..".lua")
-end)
